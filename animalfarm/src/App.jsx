df29a0d219fabb8fe6fb24e2f0625153d7c90512
Tom Ainsworth <tomainsworth94@gmail.com>
2022-08-20 18:31:49 +0100

    Add Animal component and map over it in App.jsx to show animals based on user input

--- animalfarm/src/App.jsx
+++ animalfarm/src/App.jsx
@@ -1,9 +1,45 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import Animal from './components/Animal';
 
 function App() {
-	return <div></div>;
+	const [animals, setAnimals] = useState([]);
+
+	const search = async (q) => {
+		try {
+			const response = await fetch(
+				'http://localhost:8080?' + new URLSearchParams({ q })
+			);
+			const data = await response.json();
+			setAnimals(data);
+		} catch (err) {
+			// console.log(err)
+		}
+	};
+
+	return (
+		<main>
+			<h1>Animal Farm</h1>
+
+			<input
+				type="text"
+				placeholder="Search"
+				onChange={(e) => search(e.target.value)}
+			/>
+
+			<ul>
+				{animals.length ? (
+					<>
+						{animals.map((animal) => (
+							<Animal key={animal.id} {...animal} />
+						))}
+					</>
+				) : (
+					<h2>No animals found</h2>
+				)}
+			</ul>
+		</main>
+	);
 }
 
 export default App;
